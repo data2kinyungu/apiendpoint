593987caf486162da20dc9e39ff20a0ab729a5d8
Kinyungu Denis Mashell <kinyungudenismashell@gmail.com>
2022-10-31 00:09:15 +0300

    Update serializers.py

--- taskone/counties/serializers.py
+++ taskone/counties/serializers.py
@@ -5,4 +5,4 @@ from .models import Task
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
-        fields = ["id", "slack_username", "backend", "age", "bio"]
+        fields = ["id", "slackUsername", "backend", "age", "bio"]
