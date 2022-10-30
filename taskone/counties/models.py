f85b798672c6cd9e60a2a18bd911eb416317ad0c
Kinyungu Denis Mashell <kinyungudenismashell@gmail.com>
2022-10-30 16:32:55 +0300

    Update models.py

--- taskone/counties/models.py
+++ taskone/counties/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Task(models.Model):
-    slack_username = models.CharField(max_length=100)
+    slackUsername = models.CharField(max_length=100)
     backend = models.BooleanField()
     age = models.IntegerField()
     bio = models.CharField(max_length=150)
